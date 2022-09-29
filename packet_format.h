b31907aab2616244a0b4e62c8dc779048ede672e
Nathan Jones <nathan.jones@mail.utoronto.ca>
2022-09-29 00:24:19 -0400

    packet_format commenting

--- packet_format.h
+++ packet_format.h
@@ -1,7 +1,16 @@
 struct packet {
+    //indicates the total number of fragments of the file
     unsigned int total_frag;
+
+    // sequence number of the fragment
     unsigned int frag_no;
+
+    //size of the data in the range of 0 to 1000
     unsigned int size;
+
+    //ex: “foobar.txt”
     char* filename;
+
+    //ex: “lo World!\n”
     char filedata[1000];
 }
