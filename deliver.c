2fbd2fbdb1c5ed2949f7de42562874a3f0aab5d9
Sadra Tonekaboni <sadra.tonekaboni@mail.utoronto.ca>
2022-09-29 04:17:25 -0400

    sending packets implemented

--- deliver.c
+++ deliver.c
@@ -11,20 +11,21 @@ a. if the message is “yes”, print out “A file transfer can start.”
 b. else, exit*/
 
 //Include statements
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdbool.h>
 //Networking include statements
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
 #include <netdb.h>
-
+#include "packet_format.h"
 //Maximum packet size
 #define BUFFER_SIZE 4096
-
+#define FRAGMENT_SIZE 1000
 //Temp Main
 void main(int argc, char const * argv[]){
 
@@ -33,15 +34,62 @@ void main(int argc, char const * argv[]){
 		return;
   	}
 	
-	char FileName[256];
+	char FileName[255];
+	char * mssg;
+    FILE *binary_file;
+
     printf("Enter file name: ");
     scanf("%s", FileName);
-    //fget(FileName,256,stdin);
-    printf("\n Filename: ++%s++",FileName);
+    printf("\n Filename: ++%s++\n",FileName);
+
+
+	char FilePath [263];
+	strcpy(FilePath, "./deliver_/" );
+	strcat(FilePath,FileName);
+	printf("\n FilePath: ++%s++",FilePath);
+
+
+	if (binary_file = fopen(FilePath,"r"))
+    {
+        mssg = "ftp"; 
+		printf("\nFile exists");
+    }
+    else 
+	{
+		printf("\nFile does not exists");
+        exit(1);
+	}
+
+	binary_file = fopen(FilePath,"rb");
+	fseek(binary_file, 0L, SEEK_END);
+  
+    long int size = ftell(binary_file);
+
+	int num_packets = (size + FRAGMENT_SIZE-1) / FRAGMENT_SIZE;
+
+	int offset = size%FRAGMENT_SIZE;
+	if (offset = 0)
+		offset = FRAGMENT_SIZE;
+
+	char fragment[FRAGMENT_SIZE];
+
+	char * output;
+
+	struct packet_format Packet;
+	//memset(&Packet, 0, sizeof(Packet)); 
+	Packet.total_frag = num_packets;
+	Packet.frag_no = 0;
+	Packet.size = 0;
+	Packet.filename = FileName;
+	//strcpy(Packet.filename,FileName);
+
 	
+
+
+
 	//Read server address
 	//char * s_addr = 0;
-	printf("s_addr: %d \n", argv[1]);
+	printf("\ns_addr: %d \n", argv[1]);
 	//s_addr = argv[1];
 
 	//Read port argument
@@ -88,33 +136,88 @@ void main(int argc, char const * argv[]){
 		perror("socket creation failed"); 
         exit(1);
     } 
-    char mssg_IN[BUFFER_SIZE];
-	bzero(mssg_IN, BUFFER_SIZE);
-	char * mssg;
-    FILE *file;
-    if (file = fopen(FileName,"r"))
-    {
-        mssg = "ftp"; 
-    }
-    else 
-        exit(1);
+    char mssg_IN[20];
+	bzero(mssg_IN, 20);
 	
+    
+
+
 	
 	sendto(sockfd, (const char *)mssg, strlen(mssg), 
             MSG_CONFIRM, servinfo->ai_addr,  
             servinfo->ai_addrlen);
             socklen_t servlen = sizeof(servaddr);      
-     
-    if (recvfrom(sockfd, mssg_IN, BUFFER_SIZE, 0, (struct sockaddr*)&servaddr, &servlen) < 0) {
+    printf("FTP message sent\n");
+
+    if (recvfrom(sockfd, mssg_IN, 20, 0, (struct sockaddr*)&servaddr, &servlen) < 0) {
 			printf("Message was not recieved\n");
 			exit(1);
 	}
 	else 
-	    printf(mssg_IN);
+	    printf("received: %s\n",mssg_IN);
     if (strcmp("yes", mssg_IN)==0) 
-        printf("A file transfer can start."); 
+        printf("A file transfer can start.\n"); 
     else 
         exit (1);
+
+	bool re_started = false;
+	int i = 0;
+	printf("loop begins%d\n",i);
+	printf("loop begins%d\n",num_packets); 
+	while ( i<num_packets ){	
+	printf("loop begins%d\n",num_packets); 
+	printf("loop begins%d\n",num_packets); 
+	printf("loop begins%d\n",num_packets); 
+	printf("loop begins%d\n",num_packets); 
+	printf("loop begins%d\n",num_packets); 
+	printf("loop begins%d\n",num_packets); 
+
+	//printf("loop itteration: %d", i); 
+		if (!re_started)
+		{
+			printf("loop itteration");
+			if (i < num_packets-1)
+			{
+				fread(fragment,sizeof(fragment),1,binary_file);
+				sprintf(output,"%d%d%d%s%s",num_packets,i+1,FRAGMENT_SIZE,FileName,fragment);
+			}
+
+			else 
+			{
+				fread(fragment,offset,1,binary_file);
+				sprintf(output,"%d%d%d%s%s",num_packets,i+1,offset,FileName,fragment);
+			}
+			printf("message changed\n");
+		}
+
+		printf("sending...\n");
+		sendto(sockfd, (const char *)output, strlen(output), 
+            MSG_CONFIRM, servinfo->ai_addr,  
+            servinfo->ai_addrlen);
+		printf("message sent\n");
+
+		if (recvfrom(sockfd, mssg_IN, 20, 0, (struct sockaddr*)&servaddr, &servlen) < 0) 
+		{
+			printf("Message was not recieved\n");
+			re_started = true;
+		}
+		else
+		{
+			if (strcmp("yes", mssg_IN)==0) 
+			{
+        		printf("A packet transfer was confirmed."); 
+				i++;
+				re_started = false;
+			}
+    		else 
+			{
+				printf("A packet transfer was not confirmed.");
+				re_started = true;
+			}
+		}
+
+
+	}
 
 	printf("Hello World 2");
 	return;
